5e602c305905783da157b89237e050570ca3d878
Daniil <daniilafendulov@bk.ru>
2022-06-02 01:28:20 +0300

    add key props for BottomPanel

--- src/components/TheoryTest/BottomPanel.jsx
+++ src/components/TheoryTest/BottomPanel.jsx
@@ -5,7 +5,7 @@ import Styles from '../../styles/TheoryTests/bottom-panel.module.scss';
 function BottomPanel({buttonsCallbacks, time}) {
   return (
     <div className={Styles.container}>
-      <div>{ buttonsCallbacks.map((bc,i) => <NumericButton text={1+i} onClick={bc}/>)}</div>
+      <div>{ buttonsCallbacks.map((bc,i) => <NumericButton key={i} text={1+i} onClick={bc}/>)}</div>
       <div><Timer time={time}/></div>
     </div>
   )
